6f81788c16e8e9cd08ffcce6638c1e1ca8acf1d6
smizy <shimizu@octetcode.com>
2016-09-06 16:06:36 +0900

    fix unable to remove container on circleci

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,11 +1,6 @@
 
-setup() {
-  docker network create vnettest
-  docker run --name zookeeper-1 --hostname zookeeper-1.vnettest --net vnettest -d smizy/zookeeper -server 1 1 vnettest 
-  sleep 3
-}
+# setup() {
+# }
 
-teardown() {
-  docker rm -fv zookeeper-1
-  docker network rm vnettest
-}
+# teardown() {
+# }
